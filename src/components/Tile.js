e2087bf7c2b90d37926c6e8704bc7876a6a58518
Nick Hamilton <nick.hamilton@shaw.ca>
2023-09-18 17:02:45 -0700

    Placeholder component and css

---
+++ src/components/Tile.js
@@ -0,0 +1,22 @@
+import './Tile.css';
+
+function Tile() {
+    return (
+      <div className="Tile">
+        <h1>
+            <a href="https://www.google.com">Connect 4</a>
+        </h1>
+        <p>
+            One sentence of the technology
+        </p>
+        <ol>
+            <li>Java</li>
+            <li>Poo-thon</li>
+            <li>AWS</li>
+        </ol>
+        <a href="https://www.google.com">Src</a>
+      </div>
+    );
+  }
+  
+  export default Tile;
