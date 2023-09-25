bc0622599760998b1f4c62ea67cd203d91fa087c
Nick Hamilton <nick.hamilton@shaw.ca>
2023-09-24 19:28:57 -0700

    Fixed positioning of GalleryHeader, started adding content to JS

---
+++ src/content/projects.js
@@ -0,0 +1,9 @@
+projects = [
+    {
+        "name": "Distributed Key-Value Store",
+        "short_description": "Created/deployed distributed KV store with 40+ nodes, able to concurrently handle 2048+ clients",
+        "long_description": "Did a lot more stuff",
+        "tools": ["Java, AWS"], // should I make this an enum of tools I have used ?
+        "date": new Date(2023, 3)
+    }
+]
