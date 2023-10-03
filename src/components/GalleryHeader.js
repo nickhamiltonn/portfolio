5e76feea159838f704a3003eea6ba9598a88a7b2
Nick Hamilton <nick.hamilton@shaw.ca>
2023-10-03 16:11:40 -0700

    Added code to parse and display filter options from Tags enum

--- src/components/GalleryHeader.js
+++ src/components/GalleryHeader.js
@@ -1,4 +1,21 @@
 import './GalleryHeader.css';
+import Tags from '../content/enums';
+
+const generateFilterOptions = () => {
+  var filtersJSX = []
+  Object.entries(Tags).forEach(([k,v]) => {
+    filtersJSX.push(<span className="Category">{k}</span>);
+    Object.values(v).forEach(tool => {
+      filtersJSX.push(
+        <label class="container">{tool}
+          <input type="checkbox" />
+          <span class="checkmark"></span>
+        </label>
+      );
+    });
+  });
+  return filtersJSX;
+}
 
 function GalleryHeader() {
     return (
@@ -12,7 +29,16 @@ function GalleryHeader() {
         <div className="Hover Filter">
           Filter By...
           <div className="Dropdown Filter">
-            This is where we select filters
+            <label className="Radio">
+              <input type="radio"/>
+              Any of...
+            </label>
+            <b>or</b>
+            <label className="Radio">
+              <input type="radio" />
+              All of...
+            </label>
+            {generateFilterOptions()}
           </div>
         </div>
       </div>
--- src/components/TileGallery.js
+++ src/components/TileGallery.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Tile from './Tile';
 import './TileGallery.css';
 import GalleryHeader from './GalleryHeader';
@@ -5,18 +7,20 @@ import GalleryHeader from './GalleryHeader';
 const renderTiles = (projects) => {
   var projectsJSX = []
   projects.forEach(project => {
-    projectsJSX.push(<Tile project={project}/>)
+    projectsJSX.push(<Tile project={project}/>);
   });
   return projectsJSX;
 }
 
 // need to add state to make filters and stuff work
 function TileGallery(props) {
+  const [projects, setProjects] = useState(props.projects)
+  
   return (
     <div className="TileGallery">
       <GalleryHeader />
       <div className="ListOfTiles">
-        {renderTiles(props.projects)}
+        {renderTiles(projects)}
       </div>
     </div>
   );
